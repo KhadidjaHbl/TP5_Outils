bb24fc0e8e330a5c4990929fad69984358f3070b
unknown <hk_hab_el_hames@esi.dz>
2021-01-28 10:29:41 +0100

    gerer conflit

--- Telly/src/com/telly/controllers/UserController.java
+++ Telly/src/com/telly/controllers/UserController.java
@@ -62,6 +62,42 @@ public class UserController {
 		return "home";
 
 	}
+
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
+		
+
+    @RequestMapping("/createaccount")
+	public String createAccount(Model model, Principal principal) {
+		
+		model.addAttribute("user", new User());
+		
+		return "createaccount";
+	}
+
+	@RequestMapping(value = "/createuser", method = RequestMethod.POST)
+	public String createUser(@Validated(FormValidationGroup.class) User user, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "createaccount";
+		}
+		
+		user.setAuthority("ROLE_USER");
+		user.setEnabled(true);
+
+		userService.create(user);
+		
+		return "home";
+
+	}
 	
 
 }
